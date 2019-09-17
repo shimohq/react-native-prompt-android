12ec4141639c8c93cdd701cf981c1e4af98c9426
Bell <bell@greedlab.com>
2019-09-17 11:31:38 +0800

    feature: adaptive react-native 0.60 (#52)

--- android/src/main/java/im/shimo/react/prompt/RNPromptFragment.java
+++ android/src/main/java/im/shimo/react/prompt/RNPromptFragment.java
@@ -5,11 +5,12 @@ import android.app.DialogFragment;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
 import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.WindowManager;
 import android.widget.EditText;
+
+import androidx.appcompat.app.AlertDialog;
 
 import javax.annotation.Nullable;
 
